e9954f383830bc94ac176c79c41b8d1f8d2deaf1
Ashley Carter <acarter@myocv.com>
2020-01-14 16:11:46 -0600

    Update func to control questions

--- quizApp/ViewController.swift
+++ quizApp/ViewController.swift
@@ -15,15 +15,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
+    // This label will tell us if we chose the correct or incorrect answer.
+    @IBOutlet weak var answerKeyLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .lightGray
         makeConstraints()
+        checkAnswer1()
+        question1()
+        question2()
         
     }
     
     func makeConstraints() {
-        questionLabel.text = "Is Auburn the best college ever?"
+        answerKeyLabel.text = nil
+        answerKeyLabel.textColor = .red
+        answerKeyLabel.font = UIFont.boldSystemFont(ofSize: 40)
+        checkAnswer1()
+
         questionLabel.font = UIFont.boldSystemFont(ofSize: 25)
         questionLabel.numberOfLines = 0
         questionLabel.textAlignment = .center
@@ -43,7 +53,7 @@ class ViewController: UIViewController {
         falseButton.backgroundColor = .red
         falseButton.tintColor = .white
         falseButton.layer.cornerRadius = 12.0
-        
+
         trueButton.snp.makeConstraints { (make) in
             make.top.equalTo(questionLabel.snp.bottom).offset(50)
             make.height.equalTo(60)
@@ -55,7 +65,39 @@ class ViewController: UIViewController {
             make.height.width.equalTo(trueButton)
             make.right.equalToSuperview().offset(-40)
         }
+
+        answerKeyLabel.snp.makeConstraints { (make) in
+            make.centerX.centerY.equalToSuperview()
+        }
         
+    }
+
+    func checkAnswer1() {
+        if trueButton.isTouchInside {
+            answerKeyLabel.text = "Correct. WAR EAGLE!"
+        } else if falseButton.isTouchInside {
+            answerKeyLabel.text = "Incorrect... Auburn is the best."
+        }
+    }
+
+    func question1() { // false
+        questionLabel.text = "Everyone loves our current president, Trump!"
+
+        if trueButton.isTouchInside {
+            answerKeyLabel.text = "Incorrect."
+        } else if falseButton.isTouchInside {
+            answerKeyLabel.text = "Correct. You are one smart cookie"
+        }
+    }
+    func question2() { // false
+        questionLabel.text = "Aubie is the best mascot out there."
+
+        if trueButton.isTouchInside {
+            answerKeyLabel.text = "Incorrect"
+        } else if falseButton.isTouchInside {
+            answerKeyLabel.text = "Correct. You are a smarty pants!"
+        }
+
     }
 
 
